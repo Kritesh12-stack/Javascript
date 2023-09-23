efe2d95da7695ab67a7323e4eeeb31aceefb15b7
Kritesh-Shrivastava <kriteshshri@gmail.com>
2023-09-23 00:57:14 +0000

    datatypes_done

---
+++ datatypes.js
@@ -0,0 +1,16 @@
+"use strict"
+console.log(3+3)
+console.log("Kritesh")
+
+
+let name = "Kritesh";  // String
+let age = 21; // number if bigger than 2**53 use "bigint"
+let isLoggedIn = false; // boolean
+let temperature = null; //null
+let position; //undefined
+
+let obj = {a:1,
+           b:2} // object
+
+console.table([obj,age,isLoggedIn,temperature,position])
+
