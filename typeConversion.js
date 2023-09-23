3a90981dd78dfd91904ee37cfc7484a73d910b66
Kritesh-Shrivastava <kriteshshri@gmail.com>
2023-09-23 01:25:22 +0000

    Added typeConversion to the repo

---
+++ typeConversion.js
@@ -0,0 +1,35 @@
+let a = "33";
+let orignalA = Number(a);
+console.log(typeof orignalA);
+console.log("----------------------------------------------------------------------------")
+let b = 34;
+let stringB = String(b);
+console.log(typeof stringB);
+console.log("----------------------------------------------------------------------------")
+let c = "";
+let bollc = Boolean(c);
+console.log(bollc)
+console.log("----------------------------------------------------------------------------")
+
+
+let C = "value"
+let bollC = Boolean(C);
+console.log(bollC);
+console.log("----------------------------------------------------------------------------")
+
+
+
+// Exceptional Cases
+let d = "33abc"
+let numberD = Number(d);
+console.log(numberD);
+console.log(typeof numberD);
+console.log("----------------------------------------------------------------------------")
+
+let e = null;
+let numberE = Number(e);
+console.log(numberE);
+console.log(typeof numberE);
+console.log("----------------------------------------------------------------------------")
+
+// many more too come.......................
