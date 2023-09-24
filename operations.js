d034a978a524cd97929bf87cc3e36a5fb343174c
Kritesh-Shrivastava <kriteshshri@gmail.com>
2023-09-24 02:27:36 +0000

    Conversion_Operations

---
+++ operations.js
@@ -0,0 +1,26 @@
+let val = 3;
+let negVal = -val;
+console.log(negVal);
+
+console.log("*********************************************************************************************")
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%3);
+console.log(3%2);
+
+console.log("*********************************************************************************************")
+
+console.log(1+"2");
+console.log("2"+1);
+console.log(1+2+"3");
+console.log("3"+2+1);
+
+console.log("*********************************************************************************************")
+
+console.log(true);
+console.log(+true);
+console.log(-"");
