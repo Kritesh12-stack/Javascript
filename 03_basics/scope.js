879c01778a9b8513fdd6c3a442950497198f538e
Kritesh-Shrivastava <kriteshshri@gmail.com>
2023-09-30 02:03:04 +0000

    nested scope and hoisting understanding

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -9,3 +9,42 @@ if(true){
 // console.log(a); gives error "Which is good" BLOCK scope
 // console.log(b); gives error "Which is good" BLOCK scope
 console.log(c);  // has global scope despite being declared in the block
+
+function one(){
+    const userName = "Kritesh";
+    function two(){
+        const website = "Youtube";
+        console.log(userName); // block scope can take variable from its global scope
+    }
+    // console.log(website)  gives error scope in not defined
+
+    two();
+}
+
+one();
+
+const userName = "Kritesh";
+if(userName === "Kritesh"){
+    const website = "Youtube";
+    if(website === "Youtube"){
+        console.log(`${userName} ${website}`);
+    } 
+}
+
+// hoisting
+
+
+
+
+
+
+console.log(addOne(5))
+function addOne(num){
+    return num + 1;
+}
+// console.log(addTwo); gives error cause variable names function 
+const adding = function addTwo(num){
+    return num + 2;
+}
+console.log(adding(4));
+
