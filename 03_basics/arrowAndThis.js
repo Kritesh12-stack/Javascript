49c7c62e32e6ef341eab845f149e162f80e73c99
Kritesh-Shrivastava <kriteshshri@gmail.com>
2023-10-01 07:07:52 +0000

    completed arrows and this concepts for the basics

--- 03_basics/arrowAndThis.js
+++ 03_basics/arrowAndThis.js
@@ -1,25 +1,64 @@
 const user = {
     userName : "Kritesh",
     bandWidth : 999,
-    welcome :function(){
+    welcomeMessage : function(){
         console.log(this);
-
-        return `${this.userName} welocme`
+        return `${this.userName}, Welcome to the Web` 
     }
 }
 
-console.log(user.welcome());
-user.userName = "Kritu";
-console.log(user.welcome());
+// console.log(user.welcomeMessage());
+// user.userName = "Kritu";
+// console.log(user.welcomeMessage());
 
-console.log(this);
+// console.log(this);
 
-function addTwo(){
-    userName = "Kritesh";
-    console.log(this.userName);  // not working
-}
+// function test(){
+//     console.log(this);
+// }
+// // test();
 
-const addTo = function(){
-    userName = "Kritesh";
-    console.log(this.userName);  // not working
-}
+// const test = function(){
+//     return this;
+// }
+// console.log(test())
+
+// const test = () =>{
+//     console.log(this);
+// }
+// test();
+
+// -------------------now assigning operations in functions along with this
+
+// function test(){
+//     let userName = "Kritesh";
+//     return `${this.userName}`;
+// }
+// test();
+
+// const test = function(){
+//     let userName = "Kritesh";    
+//     return `${this.userName}`;
+// }
+// console.log(test());
+
+// if we dont use let,var const if will print the userName coz at that time it will be a global variable 
+
+// Arrow
+
+// const test = (...num1) => {
+//     return num1
+// }
+// console.log(test(3,4,5,6,7,7));
+
+// const test = (...num1) =>  num1;
+// console.log(test(3,4,5,6,7));
+
+// const test = (...num1) => num1;
+// console.log(test(3,4,5,6,7));
+
+// const test = (...num1) => (num1)
+// console.log(test(3,4,5,6,7));
+
+// const test = () => ({userName : "Kritu"});
+// console.log(test());
