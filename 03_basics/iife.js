1c08be9e1eb3dd26b707ab3dae86a5880a95e085
Kritesh-Shrivastava <kriteshshri@gmail.com>
2023-10-02 01:35:40 +0000

    iife added

---
+++ 03_basics/iife.js
@@ -0,0 +1,10 @@
+ (function test(){
+    // named iife
+    console.log(`DATABASE CONNECTED`)
+ })(); //; is required in case of ending iife
+
+ ((name)=>{
+    console.log(`${name} is connected`)
+ })("Kritesh");
+
+
