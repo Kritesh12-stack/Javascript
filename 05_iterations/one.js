4c31b7ee2b6665a3096b4c9ac5ac2f622d13ca97
Kritesh-Shrivastava <kriteshshri@gmail.com>
2023-10-04 05:33:08 +0000

    added iterations (for loop concepts)

---
+++ 05_iterations/one.js
@@ -0,0 +1,41 @@
+// for (let i = 0; i <=10; i++) {
+//     console.log(i);
+    
+// }
+
+// for(let i=1;i<=10;i++){
+//     console.log(`Table of : ${i}`);
+//     for(let j=1;j<=10;j++){
+//         console.log(`${i} X ${j} = ${i*j}`);
+//     }
+// }
+
+// const arr = new Array;
+// arr.push("Batman");
+// arr.push("SuperMan");
+// arr.push("Flash")
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//     console.log(element);
+// }
+
+// for (let i = 1; i <=10; i++) {
+//     if(i==5){ 
+//     console.log(`${i} is out of stock`) 
+//     continue;
+//     }
+//     console.log(i);
+    
+// }
+
+
+// for (let i = 1; i <=10; i++) {
+//     if(i==5){ 
+//     console.log(`${i} is and after this are all out of stock`) 
+//     break;
+//     }
+//     console.log(i);
+    
+// }
+
+
