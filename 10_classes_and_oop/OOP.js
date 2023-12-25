4ec35d9c7b7d30490bc53892f56698e4a104f87b
Kritesh-Shrivastava <kriteshshri@gmail.com>
2023-12-25 07:35:14 +0000

    started oops learned new

---
+++ 10_classes_and_oop/OOP.js
@@ -0,0 +1,30 @@
+// const user = {
+//     userName : "Kritesh",
+//     loginCount : 8,
+//     isLoggedIn : true,
+
+//     nameLength : function(){
+//         console.log(this.userName.length)
+//     }
+// }
+
+// console.log(user.nameLength())
+
+
+function createUSer(username,loginCount,isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    return this
+}
+
+const user1 = new createUSer("Kritesh",8,true)
+const user2 = new createUSer("Kritu",9,false);
+// console.log(user1.constructor)
+
+function addNum(num1,num2){
+    return num1 + num2
+}
+
+console.log(addNum(1,2))
---
+++ 10_classes_and_oop/notes.md
@@ -0,0 +1,24 @@
+# javascript and classes 
+
+## OOP
+
+## Object
+- collection of properties and methods
+-eg: toLowerCase
+
+## why use OOP
+avoid spegetti code
+
+## parts of OOPs
+-object literal
+
+-Constructor function
+-Prototypes
+-Classes
+-Tnstances(new, this)
+
+## 4 pillars
+Abstraction 
+Encapsulation 
+inheritence
+polymorphism
