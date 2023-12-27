8b7a29ed8faecfc1870949113fa3bd617626e73c
Kritesh-Shrivastava <kriteshshri@gmail.com>
2023-12-27 06:44:50 +0000

    getter setters done

--- 10_classes_and_oop/modernNTraditional_get_set.js
+++ 10_classes_and_oop/modernNTraditional_get_set.js
@@ -1,15 +1,38 @@
-class User{
-    constructor(email,password){
-        this.email = email;
-        this.password = password   
-    }
-    get email(){
-        return this._email.toUpperCase()
-    }
-    set email(value){
-        this._email = value
-    }
+// class User{
+//     constructor(email,password){
+//         this.email = email;
+//         this.password = password   
+//     }
+//     get email(){
+//         return this._email.toUpperCase()
+//     }
+//     set email(value){
+//         this._email = value
+//     }
+// }
+
+// const user = new User("Kritesh@123.com",1234)
+// console.log(user.email);
+
+// function User(email,password){
+//     this.email = email;
+//     this.password = password;
+
+
+
+function User(email, password) {
+    this._email = email;  // Use a different name for the actual data property
+    this.password = password;
+
+    Object.defineProperty(this, "email", {
+        get: function () {
+            return this._email.toUpperCase();
+        },
+        set: function (value) {
+            this._email = value;
+        }
+    });
 }
 
-const user = new User("Kritesh@123.com",1234)
+const user = new User("Kritesh@123.com", 1234);
 console.log(user.email);
